5c2761a8c83628aec16bea2a3427b5cf0c22051a
Demicchi <m@v.nu>
2016-11-04 02:14:17 +0900

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var config = require('../config.json');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(config.database);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    db.serialize(function() {
+        db.all("SELECT * FROM trip", function(err, rows){
+            if (!err) {
+                res.render('index', {
+                    appName: config.appName,
+                    trips: rows
+                });
+            }
+        });
+    });
 });
 
 module.exports = router;
