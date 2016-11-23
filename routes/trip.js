27d20f734aee81e1e78ae864574c6c23ccad3325
TTY Demicchi <m@v.nu>
2016-11-23 16:30:47 +0900

    use wait.for module

--- routes/trip.js
+++ routes/trip.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var config = require('../config.json');
+var sleep = require('sleep-async')();
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(config.database);
 
@@ -18,6 +19,7 @@ router.get('/:id', function(req, res, next) {
 			}
 		});
 		
+		var queue = 0;
 		db.each("SELECT payment.id payment_id," +
 			"payment.description description, " +
 			"member.name payer_name, " +
@@ -26,20 +28,25 @@ router.get('/:id', function(req, res, next) {
 			"ON payment.payer = member.id " +
 			"WHERE payment.trip_id = ?", req.params.id, function(err, payment) {
 			if (!err) {
+				queue++;
 				db.all("SELECT member_id FROM defrayer WHERE payment_id = ?", payment.payment_id, function(err, rows) {
 					payment.defrayers = [];
 					members.forEach(function(member) {
 						var found = rows.some(function(element, index, array) {
 							return (member.id == element.member_id);
 						});
-						payment.defrayers.push((found) ? "○" : "　");
+						payment.defrayers.push((found) ? true : false);
 					});
 					payments.push(payment);
+					queue--;
 				});
 			}
 		}, function(err, num) {
-			// close db to wait for all queries finished
-			db.close(function(err) {
+			sleep.sleepWithCondition(function() {
+				return queue == 0;
+			},
+			10000,
+			function() {
 				res.render('trip', {
 					appName: config.appName,
 					members: members,
